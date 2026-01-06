d5bcfb66d4156ace0501ff660a35a101e70a3b02
Alex <oleksiymosiychuk108@gmail.com>
2026-01-07 02:13:38 +0300

    feat (courses/serializers.py): update Course serializer with field lessons

--- courses/serializers.py
+++ courses/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(serializers.ModelSerializer):
 
     class Meta:
@@ -11,17 +18,11 @@ class CourseSerializer(serializers.ModelSerializer):
 
 class CourseRetrieveSerializer(serializers.ModelSerializer):
     lessons_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(source="lesson_set.all", many=True)
 
     def get_lessons_count(self, obj):
         return obj.lesson_set.all().count()
 
     class Meta:
         model = Course
-        fields = "__all__"
-
-
-class LessonSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Lesson
         fields = "__all__"
