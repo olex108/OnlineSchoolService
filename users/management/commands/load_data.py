c0405bcd78c5c6c194a6c610a2751c3ab402a3a0
Alex <oleksiymosiychuk108@gmail.com>
2026-01-07 11:36:21 +0300

    refactor (commands/): refactor add_payment_data to two different commands

---
+++ users/management/commands/load_data.py
@@ -0,0 +1,53 @@
+from django.core.management import BaseCommand, call_command
+
+from users.models import User
+from courses.models import Course, Lesson
+
+import json
+from config.settings import BASE_DIR
+
+
+class Command(BaseCommand):
+    help = "Loads data from JSON fixtures"
+
+    def handle(self, *args, **options):
+        """
+        Method to load data from JSON fixtures
+
+        Get list of pk of users from JSON fixtures file, del users with pk from list
+        Add users from JSON fixtures file
+        Load courses and lessons data from JSON fixtures files
+        """
+
+        # Get users from fixtures
+        path_to_file = BASE_DIR / "fixtures" / "users_fixtures.json"
+        with open(path_to_file) as json_file:
+            users_data = json.load(json_file)
+
+        # Delete users with pk from fixtures
+        users_pk_list = [user["pk"] for user in users_data]
+        for pk in users_pk_list:
+            try:
+                User.objects.get(pk=pk).delete()
+            except Exception as e:
+                print(e)
+
+        # Add users from fixtures
+        for user in users_data:
+            new_user = User.objects.create(
+                id=user["pk"],
+                email=user["fields"]["email"],
+                first_name=user["fields"]["first_name"],
+                is_active=user["fields"]["is_active"],
+            )
+            new_user.set_password(user["fields"]["password"])
+            new_user.save()
+
+        # Delete and add objects Courses, Lesson, Payment
+        Course.objects.all().delete()
+        Lesson.objects.all().delete()
+
+        call_command("loaddata", str(BASE_DIR / "fixtures" / "courses_fixtures.json"))
+        call_command("loaddata", str(BASE_DIR / "fixtures" / "lessons_fixtures.json"))
+
+        self.stdout.write(self.style.SUCCESS("Successfully loaded data from fixture"))
