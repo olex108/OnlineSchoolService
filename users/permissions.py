7619c9bf4ab507f1742b45002b71c691ecd4abbb
Alex <oleksiymosiychuk108@gmail.com>
2026-01-12 12:19:08 +0300

    feat(users/permissions.py): add IsOwner permission

--- users/permissions.py
+++ users/permissions.py
@@ -4,3 +4,8 @@ class IsModer(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return request.user.groups.filter(name="Модераторы").exists()
+
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.owner
