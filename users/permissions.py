5970e58456d87edc6002ce2c73263e823ce1237b
Alex <oleksiymosiychuk108@gmail.com>
2026-01-12 12:22:43 +0300

    style(permissions.py): update with linters

--- users/permissions.py
+++ users/permissions.py
@@ -1,11 +1,13 @@
 from rest_framework import permissions
 
+
 class IsModer(permissions.BasePermission):
 
-    def has_permission(self, request, view):
+    def has_permission(self, request, view) -> bool:
         return request.user.groups.filter(name="Модераторы").exists()
 
 
 class IsOwner(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
+
+    def has_object_permission(self, request, view, obj) -> bool:
         return request.user == obj.owner
