e61a624f5a5d4d94b21d705413f0ceb415bcd044
Alex <oleksiymosiychuk@gmail.com>
2026-02-24 20:22:58 +0300

    feat: add allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     "django.contrib.admin",
